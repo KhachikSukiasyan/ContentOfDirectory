44b3a45c02e27665f22f14bef74a19932818a661
Khachik Sukiasyan <khachatur125@gmail.com>
2017-01-21 01:10:59 +0400

    Initial commit

---
+++ ContentOfDirectory/ContentOfDirectory/Dir.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ContentOfDirectory
+{
+   public class Dir
+    {
+        static string tempStr;
+        public static void printContent(string input)
+        {
+            try
+            {
+                if (!Directory.Exists(input))
+                {
+                    throw new DirectoryNotFoundException();
+                }
+                else
+                    helper(input,0);
+                
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Wrong input directory");
+            }
+        }
+
+        static void helper(string root,int spaceCount)
+        {
+            tempStr = "FOLDER: " + Path.GetFileName(root);
+            Console.WriteLine(tempStr.PadLeft(tempStr.Length + spaceCount, ' '));
+
+            spaceCount++;
+            foreach (string temp in Directory.GetFiles(root))
+            {
+                tempStr = Path.GetFileName(temp);
+                Console.WriteLine(tempStr.PadLeft(tempStr.Length + spaceCount, ' '));
+            }
+            
+            foreach (string temp in Directory.GetDirectories(root))
+            {
+                helper(temp,spaceCount + 1);
+            }
+        }
+
+    }
+}
---
+++ ContentOfDirectory/ContentOfDirectory/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentOfDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dir.printContent(Console.ReadLine());
+            Console.ReadKey();
+        }
+    }
+}
